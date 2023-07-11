653f9b72c9b8d52df24f0d4e701104a5812f9e4f
VG-Venice <68179331+VG-Venice@users.noreply.github.com>
2023-07-11 12:49:02 +0000

    Made new templates and completed home page and created subject dashboards

--- main.py
+++ main.py
@@ -4,14 +4,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_screen():
-  return render_template('home.html', site_name="SmartyNotes")
+  return render_template('home.html', site_name="SmartDuck")
 
 @app.route('/login')
 def login():
-  return render_template('login.html', site_name="SmartyNotes")
+  return render_template('login.html', site_name="SmartDuck")
 
 @app.route('/signup')
 def signup():
-  return render_template('signup.html', site_name="SmartyNotes")
+  return render_template('signup.html', site_name="SmartDuck")
+
+@app.route('/maths')
+def maths():
+  return render_template('maths.html', site_name="SmartDuck")
+
+@app.route('/chemistry')
+def chemistry():
+  return render_template('chemistry.html', site_name="SmartDuck")
+
+@app.route('/physics')
+def physics():
+  return render_template('physics.html', site_name="SmartDuck")
+
+@app.route('/about_us')
+def about_us():
+  return render_template('about_us.html', site_name="SmartDuck")
 
 app.run(host='0.0.0.0', port=81, debug=True)
